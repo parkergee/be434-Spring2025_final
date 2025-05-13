874c2ba3fcf80b03ef447ed271fc1c59338d365c
Parker Geffre <108087102+parkergee@users.noreply.github.com>
2025-05-13 00:33:26 +0000

    Substitution.py hw

--- project/02_substitution/substitution.py
+++ project/02_substitution/substitution.py
@@ -1,70 +1,140 @@
 #!/usr/bin/env python3
 """
-Author : Add your Name <Add your email>
+Author : parker <Add your email>
 Date   : 2025-01-24
 Purpose: Add Your Purpose
 """
 
 import argparse
-
+import os
+import sys
+import random
 
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Add Your Purpose',
+        description='Caesar Shift',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument(
+        'file',
+        help='Input file',
+        metavar='FILE',
+        #nargs='?',  #how to do without args
+        type=argparse.FileType('rt'))
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
+    parser.add_argument('-s', 
+        '--seed',
+        help='Random seed for reproducible keys',
+        metavar='SEED',
+        type=int,
+        default=3)
+    
+    parser.add_argument('-d',
+        '--decode',
+        help='A boolean flag',
+        action='store_true')
+    
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+        '--outfile',
+        help='Output file',
+        metavar='FILE',
+        type= str,
+        default='')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.file.name):
+        parser.error(f"No such file or directory: '{args.file.name}'")
+
+    return args
 
 
 # --------------------------------------------------
+def gen_random(seed):
+    """Generate a random substitution cipher key"""
+    if seed is not None:
+        random.seed(seed)
+
+    # Create list of uppercase letters
+    alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    # Shuffle the alphabet to create the cipher key
+    shuffled = alphabet.copy()
+    random.shuffle(shuffled)
+
+    return ''.join(shuffled)
+
+# --------------------------------------------------
+def SUBen(seed, file):
+    """Encode text using the substitution cipher"""
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ''
+    shuff = gen_random(seed)
+    for line in file:
+        line = line.strip()
+        for char in line:
+            upper_char = char.upper()
+            if upper_char in alpha:
+                # Find position in normal alphabet and substitute with cipher character
+                index = alpha.index(upper_char)
+                result += shuff[index]
+            else:
+                # Non-alphabetic characters remain unchanged
+                result += char
+    print(result)
+    
+    return result
+
+# --------------------------------------------------
+def SUBde(seed, file):
+    """Encode text using the substitution cipher"""
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ''
+    shuff = gen_random(seed)
+    for line in file:
+        line = line.strip()
+        for char in line:
+            upper_char = char.upper()
+            if upper_char in alpha:
+                # Find position in normal alphabet and substitute with cipher character
+                index = shuff.index(upper_char)
+                result += alpha[index]
+            else:
+                # Non-alphabetic characters remain unchanged
+                result += upper_char
+    print(result)
+
+    return result
+
+# --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """ENCODE/DECODE"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    f = os.path.relpath(args.file.name)
+    file = open(f, 'rt')
+    name = os.path.basename(file.name)
+    seed = args.seed
+    if args.outfile:
+        out_f = open('./outputs/' + args.outfile +'.txt', 'wt')
+    else:
+        out_f = open('./outputs/' + name, 'wt') 
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    if args.decode:
+        out_f.write(SUBde(seed, file) + '\n')
+        #print(SUBde(seed,file))
+        out_f.close()
+        file.close()
+        #print(f'Done, wrote decoded {name} to directory "outputs".')
+    else:
+        out_f.write(SUBen(seed, file) + '\n')
+        #print(SUBen(seed, file))
+        out_f.close()
+        file.close()
+        #print(f'Done, wrote encoded {name} to directory "outputs".')
 
 
 # --------------------------------------------------
